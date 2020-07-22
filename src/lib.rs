73f502439c6baabe974459d89b5aafa8738ec399
Mingxin <zou.mingxin@gmail.com>
2020-07-22 12:07:18 +1200

    parse is_server_specific field

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,13 @@ pub struct TcfString {
     pub consent_language: [char; 2],
     pub vendor_list_version: u16,
     pub tcf_policy_version: u8,
+    pub is_service_specific: bool,
     // pub purposes_allowed: u16,
     // pub max_vendor_id: u16,
     // pub vendor_consents: Vec<bool>,
 }
 
-named!(parse_bits<(&[u8], usize), (u8, i64, i64, u16, u16, u8, (u8, u8), u16, u8)>, tuple!(
+named!(parse_bits<(&[u8], usize), (u8, i64, i64, u16, u16, u8, (u8, u8), u16, u8, u8)>, tuple!(
     take_bits!(6u8),
     take_bits!(36u8),
     take_bits!(36u8),
@@ -25,7 +26,8 @@ named!(parse_bits<(&[u8], usize), (u8, i64, i64, u16, u16, u8, (u8, u8), u16, u8
     take_bits!(6u8),
     pair!(take_bits!(6u8), take_bits!(6u8)),
     take_bits!(12u8),
-    take_bits!(6u8)
+    take_bits!(6u8),
+    take_bits!(1u8)
 ));
 
 fn from_i64_to_datetime(i: i64) -> DateTime<Utc> {
@@ -48,6 +50,7 @@ pub fn parse(input: &[u8]) -> IResult<&[u8], TcfString> {
             (language_letter_1, language_letter_2),
             vendor_list_version,
             tcf_policy_version,
+            is_service_sepecific_val,
         ),
     ) = bits(parse_bits)(input)?;
 
@@ -66,6 +69,7 @@ pub fn parse(input: &[u8]) -> IResult<&[u8], TcfString> {
             ],
             vendor_list_version,
             tcf_policy_version,
+            is_service_specific: is_service_sepecific_val == 1,
         },
     ))
 }
@@ -97,5 +101,6 @@ mod tests {
         assert_eq!(r.as_ref().clone().unwrap().1.consent_language, ['E', 'N']);
         assert_eq!(r.as_ref().clone().unwrap().1.vendor_list_version, 15);
         assert_eq!(r.as_ref().clone().unwrap().1.tcf_policy_version, 2);
+        assert!(!r.as_ref().clone().unwrap().1.is_service_specific);
     }
 }
